63d7d5b6bfee9e09056dacf3b8b95ebae6349838
urogibarni <45623822+urogibarni@users.noreply.github.com>
2020-10-17 02:57:11 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,97 @@
-def teruletAbrazolas():
-    x = 0
-    while x <= matrixMaxX:
-        y = 0
-        while y <= matrixMaxY:
-            led.plot(x, y)
-            y += 1
-        x += 1
+palyaSzelesseg = 5
+palyaMagassag = 4
 
+kepernyoSzelesseg = 5
+kepernyoMagassag = 5
 
-matrixMaxY = 0
-matrixMaxX = 0
+startX = 1
+startY = 2
+
+labdaGyorsasag = 400 # minimum: 0 - maximum: 500
+keretFenyero = 1 # minimum: 0 - maximum: 255
+
 matrixMinX = 0
 matrixMinY = 0
-matrixMaxX = 4
-matrixMaxY = 3
-startX = 0
-startY = 1
+matrixMaxX = 0
+matrixMaxY = 0
 iranyX = 1
 iranyY = 1
 labdaX = startX
 labdaY = startY
+stop = False
 
-for i in range(3):
-    teruletAbrazolas()
-    basic.pause(500)
-    basic.clear_screen()
-    basic.pause(200)
+start()  
+
+def on_button_pressed_a_b():
+    global stop
+    stop = True
+input.on_button_pressed(Button.AB, on_button_pressed_a_b)
+
+def start():
+    teruletSzamitasok()
+    for i in range(6):
+        teruletAbrazolas(i)
+        basic.pause(50)
+    for i in range(10):
+        teruletAbrazolas(9 - i)
+        basic.pause(50)
+  
+def teruletSzamitasok():
+    global kepernyoSzelesseg, kepernyoMagassag, palyaSzelesseg, palyaMagassag, matrixMinX, matrixMinY, matrixMaxX, matrixMaxY
+    matrixMinX = Math.floor(kepernyoSzelesseg/2 - palyaSzelesseg/2)
+    matrixMinY = Math.floor(kepernyoMagassag/2 - palyaMagassag/2)
+    matrixMaxX = Math.floor(matrixMinX + palyaSzelesseg - 1)
+    matrixMaxY = Math.floor(matrixMinY + palyaMagassag - 1)
+
+def teruletAbrazolas(fenyero):
+    x = matrixMinX
+    while x <= matrixMaxX:
+        y = matrixMinY
+        while y <= matrixMaxY:
+            led.plot_brightness(x, y, fenyero * 40)
+            y += 1
+        x += 1
+
+def iranyvaltasok():
+    global iranyX, iranyY, labdaX, labdaY, iranyY, matrixMinX, matrixMinY, matrixMaxX, matrixMaxY
+    if labdaX >= matrixMaxX:
+        iranyX = -1;
+    if labdaX <= matrixMinX:
+        iranyX = 1;
+    if labdaY >= matrixMaxY:
+        iranyY = -1;
+    if labdaY <= matrixMinY:
+        iranyY = 1;
+
+def keretAbrazolas():
+    global keretFenyero
+    x = matrixMinX
+    while x <= matrixMaxX:
+        if x > matrixMinX and x < matrixMaxX:
+            led.plot_brightness(x, matrixMinY, keretFenyero)
+            led.plot_brightness(x, matrixMaxY, keretFenyero)
+            x += 1
+            continue
+        y = matrixMinY
+        while y <= matrixMaxY:
+            led.plot_brightness(x, y, keretFenyero)
+            y += 1
+        x += 1
+
+def pattogoLabda():
+    global iranyX, iranyY, labdaX, labdaY, labdaGyorsasag
+    labdaX = labdaX + iranyX
+    labdaY = labdaY + iranyY
+    iranyvaltasok()
+    led.plot(labdaX, labdaY)
+    basic.pause(500 - labdaGyorsasag)
+    led.unplot(labdaX, labdaY)
 
 def on_forever():
-    # labdaX = labdaX + iranyX
-    led.plot(1, 1)
+    global stop
+    if not stop:
+        keretAbrazolas()
+        pattogoLabda()
+    else:
+        basic.show_icon(IconNames.HEART)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,35 +1,119 @@
-function teruletAbrazolas() {
+let palyaSzelesseg = 5
+let palyaMagassag = 4
+let kepernyoSzelesseg = 5
+let kepernyoMagassag = 5
+let startX = 1
+let startY = 2
+let labdaGyorsasag = 400
+//  minimum: 0 - maximum: 500
+let keretFenyero = 1
+//  minimum: 0 - maximum: 255
+let matrixMinX = 0
+let matrixMinY = 0
+let matrixMaxX = 0
+let matrixMaxY = 0
+let iranyX = 1
+let iranyY = 1
+let labdaX = startX
+let labdaY = startY
+let stop = false
+start()
+input.onButtonPressed(Button.AB, function on_button_pressed_a_b() {
+    
+    stop = true
+})
+function start() {
+    let i: number;
+    teruletSzamitasok()
+    for (i = 0; i < 6; i++) {
+        teruletAbrazolas(i)
+        basic.pause(50)
+    }
+    for (i = 0; i < 10; i++) {
+        teruletAbrazolas(9 - i)
+        basic.pause(50)
+    }
+}
+
+function teruletSzamitasok() {
+    
+    matrixMinX = Math.floor(kepernyoSzelesseg / 2 - palyaSzelesseg / 2)
+    matrixMinY = Math.floor(kepernyoMagassag / 2 - palyaMagassag / 2)
+    matrixMaxX = Math.floor(matrixMinX + palyaSzelesseg - 1)
+    matrixMaxY = Math.floor(matrixMinY + palyaMagassag - 1)
+}
+
+function teruletAbrazolas(fenyero: number) {
     let y: number;
-    let x = 0
+    let x = matrixMinX
     while (x <= matrixMaxX) {
-        y = 0
+        y = matrixMinY
         while (y <= matrixMaxY) {
-            led.plot(x, y)
+            led.plotBrightness(x, y, fenyero * 40)
             y += 1
         }
         x += 1
     }
 }
 
-let matrixMaxY = 0
-let matrixMaxX = 0
-let matrixMinX = 0
-let matrixMinY = 0
-matrixMaxX = 4
-matrixMaxY = 3
-let startX = 0
-let startY = 1
-let iranyX = 1
-let iranyY = 1
-let labdaX = startX
-let labdaY = startY
-for (let i = 0; i < 3; i++) {
-    teruletAbrazolas()
-    basic.pause(500)
-    basic.clearScreen()
-    basic.pause(200)
+function iranyvaltasok() {
+    
+    if (labdaX >= matrixMaxX) {
+        iranyX = -1
+    }
+    
+    if (labdaX <= matrixMinX) {
+        iranyX = 1
+    }
+    
+    if (labdaY >= matrixMaxY) {
+        iranyY = -1
+    }
+    
+    if (labdaY <= matrixMinY) {
+        iranyY = 1
+    }
+    
 }
+
+function keretAbrazolas() {
+    let y: number;
+    
+    let x = matrixMinX
+    while (x <= matrixMaxX) {
+        if (x > matrixMinX && x < matrixMaxX) {
+            led.plotBrightness(x, matrixMinY, keretFenyero)
+            led.plotBrightness(x, matrixMaxY, keretFenyero)
+            x += 1
+            continue
+        }
+        
+        y = matrixMinY
+        while (y <= matrixMaxY) {
+            led.plotBrightness(x, y, keretFenyero)
+            y += 1
+        }
+        x += 1
+    }
+}
+
+function pattogoLabda() {
+    
+    labdaX = labdaX + iranyX
+    labdaY = labdaY + iranyY
+    iranyvaltasok()
+    led.plot(labdaX, labdaY)
+    basic.pause(500 - labdaGyorsasag)
+    led.unplot(labdaX, labdaY)
+}
+
 basic.forever(function on_forever() {
-    //  labdaX = labdaX + iranyX
-    led.plot(1, 1)
+    
+    if (!stop) {
+        keretAbrazolas()
+        pattogoLabda()
+    } else {
+        basic.showIcon(IconNames.Heart)
+    }
+    
 })
